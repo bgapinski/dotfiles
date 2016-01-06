97bc322ea79d7ec0f7e21d027c7481054ed12f91
Brian Gapinski <bgapinski@gmail.com>
2016-01-06 17:53:50 -0600

    Start x on login

--- nested/config/fish/config.fish
+++ nested/config/fish/config.fish
@@ -1,9 +1,9 @@
 # start X at login
-# if status --is-login
-#   if test -z "$DISPLAY" -a $XDG_VTNR -eq 1
-#     exec startx -- -keeptty
-#   end
-# end
+if status --is-login
+  if test -z "$DISPLAY" -a $XDG_VTNR -eq 1
+    exec startx -- -keeptty
+  end
+end
 
 fish_vi_mode
-set -gx PATH /home/brian/.cabal/bin /home/brian/.scripts $PATH
+set -gx PATH /home/brian/.cabal/bin /home/brian/.scripts /usr/local/bin $PATH
