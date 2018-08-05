e703418ac2f672248539671f1e2bfa4cace3c366
Brian Gapinski <bgapinski@gmail.com>
2018-08-05 09:06:11 -0400

    minor terminal settings update

--- nested/config/fish/config.fish
+++ nested/config/fish/config.fish
@@ -5,5 +5,10 @@ if status --is-login
   end
 end
 
+if status --is-interactive
+    set BASE16_SHELL "$HOME/.config/base16-shell/"
+    source "$BASE16_SHELL/profile_helper.fish"
+end
+
 fish_vi_key_bindings
 set -gx PATH /home/brian/.cabal/bin /home/brian/.scripts /usr/local/bin $PATH
